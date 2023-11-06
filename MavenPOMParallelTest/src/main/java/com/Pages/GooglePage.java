aca126bc6c40391bad6a4762c054167808b2ad0b
Maheshwari <maheshwari12jun@gmail.com>
2023-11-06 11:58:03 +0530

    2st commit

--- MavenPOMParallelTest/src/main/java/com/Pages/GooglePage.java
+++ MavenPOMParallelTest/src/main/java/com/Pages/GooglePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 
 public class GooglePage extends BasePage{
 	
-	By searchBox=By.xpath("//input[@class='gLFyf gsfi']");
+	By searchBox=By.xpath("//textarea[@jsname='yZiJbe']");
 	By findButton=By.xpath("//input[@class='gNO89b' and @value='Google Search']/ancestor::div[@jsname='VlcLAe']/center/input[1]");
 	
 	public GooglePage(WebDriver driver) {
